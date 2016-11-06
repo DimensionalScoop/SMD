5ef3d6152711c3e6da57c7154dc27f7a19e9b3c0
Max Perklau <max.pernklau@udo.edu>
2016-11-06 16:46:00 +0100

    Add data, exercises

---
+++ Max/Blatt1/aufgabe_2.py
@@ -0,0 +1,10 @@
+import ROOT
+
+random_gen = ROOT.TRandom3()
+print(random_gen)
+
+from numpy import *
+
+asd
+
+sin()
---
+++ Max/Blatt1/np_test.py
@@ -0,0 +1,5 @@
+from numpy import *
+import numpy as np
+
+a = [1, 2, 4, 6, 8, 10, 11, 12, 16]
+b = array(a)
