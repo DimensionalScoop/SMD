173300c81d48b4546038f21bfb7e6a622baa06cb
Max Perklau <max.pernklau@udo.edu>
2016-11-08 02:40:11 +0100

    Finally a color contour plot for 2.4

--- Max/Blatt1/aufgabe_4.py
+++ Max/Blatt1/aufgabe_4.py
@@ -1,43 +1,56 @@
 import numpy as np
-import scipy.stats as stats
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
 import matplotlib.pyplot as plt
+import smd
 
+smd.enable_tex_for_plotting()
 
-#########
-# Ellipse ist noch nicht korrekt! Wir haben die Formel für die unkorrelierte Gaussian benutzt!
-#########
-
-
-plt.rc('text', usetex=True)
-plt.rc('font', family='serif')
 
 μ = np.array((4, 2))
 σ = np.array((3.5, 1.5))
 cov = 4.2
-
-k_0 = 1 / (2 * np.pi * np.prod(σ))
+# NO k_0 = 1 / (2 * np.pi * np.prod(σ))
 
 ρ = cov / np.prod(σ)
 print("a)", ρ)
 
 
-def gauß_exp(x, σ, μ):
-    """Return exponent of a Gaussian"""
-    return (x - μ)**2 / σ**2
+# covariance matrix
+Σ = np.array([
+    [smd.variance_gauss(σ[0], μ[0]), cov],
+    [cov, smd.variance_gauss(σ[1], μ[1])]])
+
 
 # Generate mesh for b)
-x = np.linspace(4 - 5, 4 + 5, 1000)
-y = np.linspace(2 - 5, 2 + 5, 1000)
+x = np.linspace(4 - 10, 4 + 10, 100)
+y = np.linspace(2 - 10, 2 + 10, 100)
 X, Y = np.meshgrid(x, y)
 
+
+def gauss(x):
+    return smd.gauss_multivariate(x, Σ, μ)
+prefactor = smd.gauss_multivariate_prefactor(Σ)
+
 # Exponent of the Gaussian
-F = k_0 * np.exp(-1 / 2 * (gauß_exp(X, μ[0], σ[0]) + gauß_exp(Y, μ[1], σ[1])))
+# F = k_0 * np.exp(-1 / 2 * (gauß_exp(X, μ[0], σ[0]) + gauß_exp(Y, μ[1], σ[1])))
+
+# Fill matrix with values of the Gaussian at different positions
+Z = np.zeros((x.size, y.size))
+F = []
+for i, f_x in enumerate(x):
+    for j, f_y in enumerate(y):
+        Z[i, j] = gauss((f_x, f_y))
+        F.append((f_x, f_y, Z[f_x, f_y]))
+F = np.array(F)
 
 # Only draw contour where F(X,Y) = 1, i.a. the ellipsis
-plt.contour(X, Y, F, [k_0 / np.sqrt(np.e)])
+#plt.contour(X, Y, F, [prefactor / np.sqrt(np.e)])
 
 plt.errorbar(*μ, xerr=σ[0], yerr=σ[1], fmt='g', label=r'$\mu \pm \sigma$')
 
-plt.legend(loc='best')
 
+plt.contourf(X, Y, Z, cmap=cm.jet)
+
+plt.legend(loc='best')
 plt.savefig("fig/4b.pdf")
---
+++ Max/Blatt1/smd.py
@@ -0,0 +1,44 @@
+import numpy as np
+import numpy.linalg as linalg
+from scipy.linalg import expm
+import matplotlib.pyplot as plt
+import scipy.integrate as integrate
+
+
+def enable_tex_for_plotting():
+    """Set up pyplot for LaTeX"""
+    plt.rc('text', usetex=True)
+    plt.rc('font', family='serif')
+
+
+def gauss(x, σ, μ):
+    """Return Gaussian pdf"""
+    return np.exp(-1 / 2 * (x - μ)**2 / σ**2) / np.sqrt(2 * np.pi * σ**2)
+
+
+def variance_gauss(σ, μ):
+    """Return variance of Gaussian"""
+    variance, error = integrate.quad(lambda x: (x - μ)**2 * gauss(x, σ, μ), -np.inf, np.inf)
+
+    assert error < 1e-7
+    return variance
+
+
+def gauss_multivariate_prefactor(Σ):
+    dim = Σ.size
+    return np.sqrt((2 * np.pi)**dim * linalg.det(Σ))
+
+
+def gauss_multivariate(x, Σ, μ):
+    x = np.array(x)
+    μ = np.array(μ)
+    dim = x.size
+    assert μ.size == dim
+    assert Σ.shape == (dim, dim)
+
+    N = gauss_multivariate_prefactor(Σ)
+    exp = -1 / 2 * np.dot(np.dot((x - μ).T, linalg.inv(Σ)), (x - μ))
+
+    result = 1 / N * np.exp(exp)
+    assert np.size(result) == 1
+    return result
