533b6b8e4d6e9756f50dd5f3f592f50db3cd873e
Jean-Marco Alameddine <jean-marco.alameddine@tu-dortmund.de>
2017-01-14 21:40:36 +0100

    Blatt 9 Aufgaben 1 und 2

---
+++ Jean-Marco/Blatt9/aufg2.py
@@ -0,0 +1,27 @@
+import numpy as np
+from numpy.linalg import inv
+
+A = np.matrix( [ [1, np.sqrt(3)/2, 1/2, 0, -1/2, -np.sqrt(3)/2, -1, -np.sqrt(3)/2, -1/2, 0, 1/2, np.sqrt(3)/2] , [0, 1/2, np.sqrt(3)/2, 1, np.sqrt(3)/2, 1/2, 0, -1/2, -np.sqrt(3)/2, -1, -np.sqrt(3)/2, -1/2] ] )
+A = np.transpose(A)
+
+tmp = np.matmul(np.transpose(A),A)
+tmpinv = inv(tmp)
+G = tmpinv * np.transpose(A)
+
+y = np.matrix( [-0.032, 0.010, 0.057, 0.068, 0.076, 0.080, 0.031, 0.005, -0.041, -0.09, -0.088, -0.074] )
+y = np.transpose(y)
+a = G*y
+print(a)
+
+
+
+### Part 2
+
+
+V = np.matrix( [ [0.00449, 0 ] , [0, 0.00449 ] ] )
+J = np.matrix( [ [0.43607, -0.89991] , [10.46304, 5.07017] ] )
+J = np.transpose(J)
+tmp2 =  np.matmul( V, np.transpose(J))
+V_neu = np.matmul( J, tmp2)
+
+print(V_neu)
---
+++ Jean-Marco/Blatt9/aufg2_test.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def f(a0, d, psi):
+    return -a0 * np.cos(psi+d)
+
+def f2(a1, a2, psi):
+    return a1 * np.cos(psi) + a2*np.sin(psi) 
+
+x = np.linspace(0, 2*np.pi)
+plt.plot(x, f(0.086, 1.11952, x))
+#plt.plot(x, f(-0.03751, 0.07740, x))
+u = np.array([0, 30, 60, 90, 120, 150, 180, 210, 240, 270, 300, 330])
+u = u/360 * 2*np.pi
+v = np.array([-0.032, 0.010, 0.057, 0.068, 0.076, 0.080, 0.031, 0.005, -0.041, -0.09, -0.088, -0.074])
+plt.plot(u,v,'k.')
+plt.show()
+
