640039ab029a05a5b423ca264a280900b686e541
Jean-Marco Alameddine <jean-marco.alameddine@tu-dortmund.de>
2017-01-17 22:00:29 +0100

    Minor Corrections 2/3

--- Jean-Marco/Blatt9/aufg2.py
+++ Jean-Marco/Blatt9/aufg2.py
@@ -3,15 +3,20 @@ from numpy.linalg import inv
 
 A = np.matrix( [ [1, np.sqrt(3)/2, 1/2, 0, -1/2, -np.sqrt(3)/2, -1, -np.sqrt(3)/2, -1/2, 0, 1/2, np.sqrt(3)/2] , [0, 1/2, np.sqrt(3)/2, 1, np.sqrt(3)/2, 1/2, 0, -1/2, -np.sqrt(3)/2, -1, -np.sqrt(3)/2, -1/2] ] )
 A = np.transpose(A)
+print("\nDesignmatrix:")
+print(A, "\n\n")
 
 tmp = np.matmul(np.transpose(A),A)
 tmpinv = inv(tmp)
+print("(ATA)^(-1):")
+print(tmpinv, "\n\n")
 G = np.matmul(tmpinv, np.transpose(A))
 
 y = np.matrix( [-0.032, 0.010, 0.057, 0.068, 0.076, 0.080, 0.031, 0.005, -0.041, -0.09, -0.088, -0.074] )
 y = np.transpose(y)
 a = np.matmul(G,y)
-print(a)
+print("Lösungsvektor:")
+print(a, "\n\n")
 
 
 
@@ -19,9 +24,10 @@ print(a)
 
 
 V = np.matrix( [ [0.00449**2, 0 ] , [0, 0.00449**2 ] ] )
-J = np.matrix( [ [0.43607, -0.89991] , [10.46304, 5.07017] ] )
+J = np.matrix( [ [0.436077, -0.89991] , [10.46304, 5.07017] ] )
 #J = np.transpose(J)
 tmp2 =  np.matmul( V, np.transpose(J))
 V_neu = np.matmul( J, tmp2)
 
+print("Neue Kovarianzmatrix in Delta und A0:")
 print(V_neu)
--- Jean-Marco/Blatt9/aufg3.py
+++ Jean-Marco/Blatt9/aufg3.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 from scipy import stats
 
 ### aufgabe 3a)
-
+print("\nAufgabe 3a:")
 def f_fit(a, x):
     summe = 0
     for i, item in enumerate(a):
@@ -22,30 +22,35 @@ x = data.values.T[0, :]
 y = data.values.T[1, :]
 
 A = np.matrix([x**0, x, x**2, x**3, x**4, x**5, x**6]).T #designmatrix
+print("Designmatrix:")
+print(A, "\n\n")
 
 aTa_trans_inv = inv(np.matmul(A.T, A))
 
 aTa_trans_inv_aT = np.matmul(aTa_trans_inv, A.T)
 
 a = np.ravel(np.matmul(aTa_trans_inv_aT, y)) #ravel to make a 1-dim array
-print(a)
+print("Koeffizienten:")
+print(a, "\n\n")
 
 val = np.linspace(0, 8, 1000)
 plt.plot(val, f_fit(a,val))
 plt.plot(x,y, 'k.')
+plt.xlabel(r'$x$')
+plt.ylabel(r'$y$')
 plt.savefig('3a.pdf')
 plt.clf()
 
 ### aufgabe 3b)
 ### nach Skript, Seite 92
-
+print("Aufgabe 3b: \n")
 # i dont really understand this C-matrix
-C = np.matrix( [[-1, 1, 0, 0, 0, 0, 0, 0], 
-                [1, -2, 1, 0, 0, 0, 0, 0], 
-                [0, 1, -2, 1, 0, 0, 0, 0], 
-                [0, 0, 1, -2, 1, 0, 0, 0], 
-                [0, 0, 0, 1, -2, 1, 0, 0], 
-                [0, 0, 0, 0, 1, -2, 1, 0], 
+C = np.matrix( [[-1, 1, 0, 0, 0, 0, 0, 0],
+                [1, -2, 1, 0, 0, 0, 0, 0],
+                [0, 1, -2, 1, 0, 0, 0, 0],
+                [0, 0, 1, -2, 1, 0, 0, 0],
+                [0, 0, 0, 1, -2, 1, 0, 0],
+                [0, 0, 0, 0, 1, -2, 1, 0],
                 [0, 0, 0, 0, 0, 1, -2, 1],
                 [0, 0, 0, 0, 0, 0, 1, -1]] )
 
@@ -61,6 +66,7 @@ for lambd in lambd_val:
 val = np.linspace(0, 8, 1000)
 for i, args in enumerate(a_list):
     plt.plot(val, f_fit(args,val), label=lambd_val[i])
+    print("Lambda = ", lambd_val[i], ":\n ", args)
 
 plt.plot(x,y, 'k.')
 plt.legend()
@@ -72,6 +78,8 @@ plt.clf()
 
 ### aufgabe 3c)
 
+print("Aufgabe 3c: \n")
+
 data_neu = pd.read_csv('aufg_c.csv')
 x = data_neu.values[:,0]
 y_means = np.array([])
@@ -82,14 +90,17 @@ for i, args in enumerate(x):
     y_means = np.append(y_means, np.mean(data_neu.values[i,1:]) )
     #x_stds = np.append(x_stds, np.std(data_neu.values[i,1:]) ) stats.sem does the same job (dont forget *1/sqrt(n) here)
     y_SE = np.append(y_SE, stats.sem(data_neu.values[i,1:]) )
-    
+
 W = np.diag(1/y_SE**2)
+print("Gewichtsmatrix W:")
+print(W, "\n\n")
 
 mat1 = np.matmul(A.T, W)
 mat2 = np.matmul(mat1, A)
 mat3 = np.matmul(inv(mat2), A.T)
 mat4 = np.matmul(mat3, W)
 a = np.ravel(np.matmul(mat4, y))
+print("Gewichte:")
 print(a)
 val = np.linspace(0, 8, 1000)
 plt.plot(val, f_fit(a,val))
@@ -97,5 +108,3 @@ plt.plot(val, f_fit(a,val))
 plt.errorbar(x, y_means, xerr=0, yerr=y_SE, fmt='k.')
 plt.savefig('3c.pdf')
 plt.clf()
-
-
